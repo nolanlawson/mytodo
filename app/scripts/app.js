af07475d16205fff255f1392f95d960d1a530c26
crullian <crullian@gmail.com>
2014-08-10 22:51:43 -0400

    Write unit tests

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,19 +16,27 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ui.sortable'
+    'ui.sortable',
+    'LocalStorageModule'
   ])
-  .config(function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+
+.config(['localStorageServiceProvider',
+  function(localStorageServiceProvider) {
+    localStorageServiceProvider.setPrefix('ls');
+  }
+])
+
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
